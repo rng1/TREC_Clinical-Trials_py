d71ec73dfbdc773503944d582fdf488d51fc38a3
rng1 <naranjogonzalezrodrigo@gmail.com>
2024-06-19 16:08:28 +0200

    commit

--- embeddings/embeddings.py
+++ embeddings/embeddings.py
@@ -1,11 +1,14 @@
 import os
+from datetime import timedelta
 
 import pandas as pd
 import torch
 from torch import nn
+from timeit import default_timer as timer
 
-TRIAL_CSV_PATH = "C:/Users/rnara/Desktop/TFG/[py] TREC_Clinical-Trials/trial_embeddings.csv"
-TOPIC_CSV_PATH = "C:/Users/rnara/Desktop/TFG/[py] TREC_Clinical-Trials/topic_embeddings.csv"
+MAIN_PATH = "C:/Users/rnara/Desktop/TFG/[py] TREC_Clinical-Trials/results/specter"
+TRIAL_CSV_PATH = MAIN_PATH + "/crit+summ+kw/trial_embeddings.csv"
+TOPIC_CSV_PATH = MAIN_PATH + "/topic_embeddings.csv"
 
 
 def get_embeddings(tokenizer, model: nn.Module, text: str) -> torch.Tensor:
@@ -48,15 +51,33 @@ def get_topic_embeddings(tokenizer, model: nn.Module, topic_csv: pd.DataFrame) -
         return generate_topic_embeddings(tokenizer, model, topic_csv)
 
 
+def print_progress_bar(iteration, total, time, length=50):
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = '█' * filled_length + '-' * (length - filled_length)
+    time_str = str(timedelta(seconds=int(time)))
+    print(f'\rProgress: {iteration}/{total}\t|{bar}| {percent}%\t{time_str}', end='')
+    if iteration == total:
+        print()
+
+
 def generate_trial_embeddings(tokenizer, model: nn.Module, trial_csv: pd.DataFrame) -> pd.DataFrame:
     data = []
     embeddings = []
+    it = 0
+    total = len(trial_csv)
+    start = timer()
     for _, trial in trial_csv.iterrows():
-        print(f"Processing trial {trial['nct_id']}...")
-        summary = trial["summary"] if pd.notna(trial["summary"]) else ""
-        description = trial["description"] if pd.notna(trial["description"]) else ""
+        it += 1
+        print_progress_bar(it, total, timer() - start)
         criteria = trial["criteria"] if pd.notna(trial["criteria"]) else ""
-        text = summary + description + criteria
+        keywords = trial["keywords"] if pd.notna(trial["keywords"]) else ""
+        conditions = trial["conditions"] if pd.notna(trial["conditions"]) else ""
+        summ = trial["summary"] if pd.notna(trial["summary"]) else ""
+
+        # FIELDS
+        text = criteria + " " + summ + " " + keywords + " " + conditions
+
         embeddings = get_embeddings(tokenizer, model, text).detach().numpy().tolist()[0]
         data_row = [trial["nct_id"]] + embeddings
         data.append(data_row)
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from adapters import AutoAdapterModel
-from transformers import AutoTokenizer
-from numpy import linalg as LA
+# from adapters import AutoAdapterModel
 
-import torch
 import numpy as np
+import torch
+from numpy import linalg as LA
+from transformers import AutoTokenizer, AutoModel
 
 from embeddings import get_trial_embeddings, get_topic_embeddings
 from filter import FilterTrials
@@ -11,10 +11,16 @@ from xmlparser import TopicParser
 from xmlparser import TrialParser
 
 
-def create_trec_eval_file(ranking_scores, ranking_ids, run_name):
-    ranking_ids = ranking_ids.to_numpy()
+SPECTER = "allenai/specter2_base"
+BIOBERT = "dmis-lab/biobert-v1.1"
+CLINICALBERT = "medicalai/ClinicalBERT"
+BIOCLINICALBERT = "emilyalsentzer/Bio_ClinicalBERT"
 
-    with open(f'py_trec_eval_{run_name.lower()}.txt', 'w') as f:
+MODEL = SPECTER
+
+
+def create_trec_eval_file(ranking_scores, ranking_ids, run_name):
+    with open(f'py_{run_name.lower()}.txt', 'w') as f:
         for i in range(ranking_scores.shape[0]):
             for j in range(ranking_scores.shape[1]):
                 line = f"{i + 1} Q0 {ranking_ids[i][j].upper()} {j + 1} {ranking_scores[i][j]} {run_name.upper()}\n"
@@ -27,12 +33,12 @@ def map_index_to_id(index):
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f"Device used : {device}")
+print(f"Model used : {MODEL}\n")
 
 if __name__ == "__main__":
     print("Loading model and tokenizer...")
-    tokenizer = AutoTokenizer.from_pretrained("allenai/specter2_base")
-    model = AutoAdapterModel.from_pretrained("allenai/specter2_base").to(device).requires_grad_(False)
-    model.load_adapter("allenai/specter2", source="hf", load_as="specter2", set_active=True)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModel.from_pretrained(MODEL).to(device).requires_grad_(False)
 
     topic_csv = TopicParser().df
     trial_csv = TrialParser().df
@@ -50,15 +56,15 @@ if __name__ == "__main__":
     topic_trial_dot_product = topics @ trials.T
     cosine_similarity = topic_trial_dot_product / (topics_norm * trials_norm)
     sorted_indices = np.argsort(cosine_similarity, axis=1)[:, ::-1]
-    capped_indices = sorted_indices[:, :1000]
+    threshold = 1000
+    capped_indices = sorted_indices[:, :threshold]
 
     vfunc = np.vectorize(map_index_to_id)
 
     unfiltered_ranking = vfunc(capped_indices)
 
     # Filtered ranking
-    sorted_capped_indices = np.zeros((50, 1000), dtype=int)
-    threshold = 1000
+    sorted_capped_indices = np.zeros((50, threshold), dtype=int)
     trial_id_to_index = {id: index for index, id in enumerate(trial_csv['nct_id'])}
     filtered_idx = {topic: [trial_id_to_index[id] for id in ids] for topic, ids in filtered_dict.items()}
 
@@ -75,5 +81,5 @@ if __name__ == "__main__":
     filtered_ranking_scores = cosine_similarity[row_indices, sorted_capped_indices]
 
     # Create the trec_eval files
-    create_trec_eval_file(unfiltered_ranking_scores, unfiltered_ranking, "unfiltered")
+    create_trec_eval_file(unfiltered_ranking_scores, unfiltered_ranking, "rocchio_test")
     create_trec_eval_file(filtered_ranking_scores, filtered_ranking, "filtered")
